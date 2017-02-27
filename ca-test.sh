47bcdb4b0b043109ec0af9a45f74a937d6a83778
Andrew Chin <git@andrewtchin.com>
2017-02-26 21:46:00 -0800

    update

--- ca-test.sh
+++ ca-test.sh
@@ -22,7 +22,7 @@ openssl genrsa -out private/ca.key.pem 4096
 chmod 400 private/ca.key.pem
 
 # Generate root CA CSR
-openssl req -config openssl.cnf\
+openssl req -config openssl.cnf \
     -new -sha256 \
     -key private/ca.key.pem \
     -out csr/ca.csr.pem \
@@ -41,84 +41,38 @@ openssl x509 -noout -text -in certs/ca.cert.pem
 openssl x509 -in certs/ca.cert.pem -inform PEM -out certs/ca.cert.crt
 
 
-### Create intermediate CA
-cd -
-mkdir -p /root/ca/intermediate
-cp openssl-intermediate.cnf /root/ca/intermediate
-cd /root/ca/intermediate
-mkdir certs crl csr newcerts private
-chmod 700 private
-touch index.txt
-echo 1000 > serial
-echo 1000 > crlnumber
-
-# Generate intermediate CA key
-cd /root/ca
-# Private key is not encrypted - use -aes256 to specify a password
-openssl genrsa -out intermediate/private/intermediate.key.pem 4096
-chmod 400 intermediate/private/intermediate.key.pem
-
-# Generate intermediate CA CSR
-openssl req -config intermediate/openssl-intermediate.cnf\
-    -new -sha256 \
-    -key intermediate/private/intermediate.key.pem \
-    -out intermediate/csr/intermediate.csr.pem \
-    -extensions v3_intermediate_ca \
-    -subj "/C=US/ST=California/L=Los Angeles/O=Stark Enterprises/OU=Stark Enterprises Certificate Authority/CN=Stark Enterprises Intermediate CA"
-
-# Sign CSR with root CA key
-openssl ca -config openssl.cnf \
-    -batch \
-    -extensions v3_intermediate_ca \
-    -days 3650 -notext -md sha256 \
-    -in intermediate/csr/intermediate.csr.pem \
-    -out intermediate/certs/intermediate.cert.pem
-
-chmod 444 intermediate/certs/intermediate.cert.pem
-openssl x509 -noout -text -in intermediate/certs/intermediate.cert.pem
-
-# Create certificate chain
-cat intermediate/certs/intermediate.cert.pem \
-    certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
-chmod 444 intermediate/certs/ca-chain.cert.pem
-
-# Output CRT format
-openssl x509 -in intermediate/certs/intermediate.cert.pem -inform PEM -out intermediate/certs/intermediate.cert.crt
-
 ### Create server certificate
 cd /root/ca
 # Generate server key
 # Private key is not encrypted - use -aes256 to specify a password
-openssl genrsa -out intermediate/private/${SERVER_CERT_CN}.key.pem 4096
-chmod 400 intermediate/private/${SERVER_CERT_CN}.key.pem
+openssl genrsa -out private/${SERVER_CERT_CN}.key.pem 4096
+chmod 400 private/${SERVER_CERT_CN}.key.pem
 
 # Generate server CSR
-openssl req -config intermediate/openssl-intermediate.cnf\
+openssl req -config openssl.cnf \
     -new -sha256 \
-    -key intermediate/private/${SERVER_CERT_CN}.key.pem \
-    -out intermediate/csr/${SERVER_CERT_CN}.csr.pem \
+    -key private/${SERVER_CERT_CN}.key.pem \
+    -out csr/${SERVER_CERT_CN}.csr.pem \
     -subj "/C=US/ST=California/L=Los Angeles/O=Stark Enterprises/OU=Stark Enterprises Web Services/CN=${SERVER_CERT_CN}"
 
-# Sign CSR with intermediate CA key, output server certificate
-openssl ca -config intermediate/openssl-intermediate.cnf \
+# Sign CSR with CA key, output server certificate
+openssl ca -config openssl.cnf \
     -batch \
     -extensions server_cert \
     -days 365 -notext -md sha256 \
-    -in intermediate/csr/${SERVER_CERT_CN}.csr.pem \
-    -out intermediate/certs/${SERVER_CERT_CN}.cert.pem
+    -in csr/${SERVER_CERT_CN}.csr.pem \
+    -out certs/${SERVER_CERT_CN}.cert.pem
 
-chmod 444 intermediate/certs/${SERVER_CERT_CN}.cert.pem
-openssl x509 -noout -text -in intermediate/certs/${SERVER_CERT_CN}.cert.pem
+chmod 444 certs/${SERVER_CERT_CN}.cert.pem
+openssl x509 -noout -text -in certs/${SERVER_CERT_CN}.cert.pem
 
 # Test certificate
-openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/${SERVER_CERT_CN}.cert.pem
+openssl verify -CAfile certs/ca.cert.pem certs/${SERVER_CERT_CN}.cert.pem
 
 
 ### Bundle output
 mkdir bundle
 cp certs/ca.cert.crt bundle
-cp intermediate/certs/intermediate.cert.crt bundle
-cp intermediate/certs/ca-chain.cert.pem bundle
-cp intermediate/private/${SERVER_CERT_CN}.key.pem bundle
-cp intermediate/certs/${SERVER_CERT_CN}.cert.pem bundle
+cp private/${SERVER_CERT_CN}.key.pem bundle
+cp certs/${SERVER_CERT_CN}.cert.pem bundle
 tar cvf cert-bundle.tgz bundle
